15004c4d793ba398728caa07f450d411446069f9
unknown <medyb.enstab@gmail.com>
2025-11-04 19:05:42 +0100

    added Dockerfile to backend and frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
+  },
 }
 
 module.exports = nextConfig
